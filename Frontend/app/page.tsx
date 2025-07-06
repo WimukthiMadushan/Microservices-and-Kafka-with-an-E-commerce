515f8a7a1a8cd034423d691941fec4e26d1d99e8
WimukthiMdaushan <bandarawgwm.21@uom.lk>
2025-07-06 12:15:50 +0530

    initialize project

---
+++ Frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
